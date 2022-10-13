dc1f7324406c458ffaffb10dfead9c7b314c6b91
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-10-13 18:29:04 -0300

    Model.

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+use seed::{prelude::*, *};
+
+// Init
+fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
+    Model {}
+}
+
+// Model
+struct Model {
+    todos: BTreeMap<Ulid, Todo>,
+    new_todo_title: String,
+    selected_todo: Option<SelectedTodo>,
+    filter: Filter,
+    base_url: Url,
+}
+
+struct Todo {
+    id: ID,
+    title: String,
+    completed: bool,
+}
+
+struct SelectedTodo {
+    id: Ulid,
+    title: String,
+    input_element: ElRef<web_sys::HtmlInputElement>,
+}
+
+enum Filter {
+    All,
+    Active,
+    Completed,
+}
